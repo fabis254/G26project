7d7a50e3c7b5259e400ef3c460acb856cd9dcc53
Tore Haglund <ytterdorr@gmail.com>
2017-12-08 22:48:14 +0100

    klurade på javascriptet

--- tores_filer/spinner_tore.js
+++ tores_filer/spinner_tore.js
@@ -1,6 +1,103 @@
 
+//###### Main-funktionen uppflyttad hit #######
+$(document).ready(function(){               
+
+    $('.wing').on('mousedown', function(event){
+        // Get mouse angle (theta) from center
+        var theta = getPositiveTheta($("#innerCircle"), event.pageX,event.pageY)
+        // Get current orientation
+        var rotation = getAngle();
+        $('body').on('mousemove', function(event){
+            // Update theta and add difference to rotation
+            var prev_theta = theta
+            theta = getPositiveTheta($("#innerCircle"), event.pageX,event.pageY)
+            rotation += (theta - prev_theta);
+            // Adjust if rotation exceeds limits
+            if (Math.abs(rotation) > 180) {
+                rotation = adjustRotation(rotation);
+            }
+            // Rotate
+            var rotate = 'rotate(' + rotation + 'deg)';
+            $("#marker").css({"transition": "none", "transform": rotate })    
+        });
+                      
+        $('body').mouseup(function(event){
+            $('body').unbind('mousemove');
+            $('body').unbind('mouseup');
+
+            var ang = getAngle();
+            var b = adjustToIncrement(ang, $('#marker'));
+            
+        });
+    });                   
+}); 
 
 
+
+function getAngle(){
+    //Calculate the angle of Marker
+    var el = document.getElementById("marker");
+    var st = window.getComputedStyle(el, null);
+    var tr = st.getPropertyValue("-webkit-transform") ||
+             st.getPropertyValue("-moz-transform") ||
+             st.getPropertyValue("-ms-transform") ||
+             st.getPropertyValue("-o-transform") ||
+             st.getPropertyValue("transform") ||
+             "FAIL";
+    var values = tr.split('(')[1].split(')')[0].split(',');
+    var a = values[0];
+    var b = values[1];    
+
+    var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+    // console.log("Rotation: " + angle + "deg");
+    return angle
+}
+
+
+function adjustToIncrement(angle, cropper){
+    // Ease into set positions
+    var increments = [0, 90, 180, -90, -180];
+    var len = increments.length
+    var dif = 99999;
+    var selected = 0;
+    for (var i = 0; i < len; i++) {
+        var comp = Math.abs(angle - increments[i])
+        if (comp < dif) {
+            dif = comp;
+            selected = increments[i]
+        }
+    }
+
+    // Smooth animation to put elements in place
+    // This code works:
+    var rotate = 'rotate(' + selected + 'deg)';
+    cropper.css({'-moz-transform': rotate, 'transform' : rotate, '-webkit-transform': rotate, '-ms-transform': rotate,
+        "transition": "1s ease-out"});
+}
+
+function getPositiveTheta(offsetSelector, xCoordinate, yCoordinate) {
+    var x = xCoordinate - offsetSelector.offset().left - offsetSelector.width()/2;            
+    var y = (yCoordinate - offsetSelector.offset().top - offsetSelector.height()/2);
+    var theta = Math.atan2(y,x)*(180/Math.PI);        
+    // add 360 to ensure positive numbers
+    return theta + 360;
+}
+
+function adjustRotation(rotation) {
+    if (rotation > 180) {
+        return rotation -= 360;
+    }
+    else if (rotation <-180) {
+        return rotation += 360;
+    }
+    else {
+        return rotation;
+    }
+}
+
+
+
+/*
 function rotateAnnotationCropper(offsetSelector, xCoordinate, yCoordinate, cropper, orientation){
         cropper.css({"transition" : "none"})
         var d = new Date();
@@ -49,80 +146,4 @@ function rotateAnnotationCropper(offsetSelector, xCoordinate, yCoordinate, cropp
         var cssDegs = 90 - theta;
         return cssDegs;
     }
-
-function getAngle(){
-    //Calculate the angle of Marker
-    var el = document.getElementById("marker");
-    var st = window.getComputedStyle(el, null);
-    var tr = st.getPropertyValue("-webkit-transform") ||
-             st.getPropertyValue("-moz-transform") ||
-             st.getPropertyValue("-ms-transform") ||
-             st.getPropertyValue("-o-transform") ||
-             st.getPropertyValue("transform") ||
-             "FAIL";
-    var values = tr.split('(')[1].split(')')[0].split(',');
-    var a = values[0];
-    var b = values[1];    
-
-    var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-    // console.log("Rotation: " + angle + "deg");
-    return angle
-}
-
-
-function adjustToIncrement(angle, cropper){
-    // Ease into set positions
-    var increments = [0, 90, 180, -90, -180];
-    var len = increments.length
-    var dif = 99999;
-    var selected = 0;
-    for (var i = 0; i < len; i++) {
-        var comp = Math.abs(angle - increments[i])
-        if (comp < dif) {
-            dif = comp;
-            selected = increments[i]
-        }
-    }
-
-    // Adjust for stupid angles
-    if (selected < 0 && angle < -90) {
-        selected += 360;
-    }
-
-    // Smooth animation to put elements in place
-    // This code works:
-    var rotate = 'rotate(' + selected + 'deg)';
-    cropper.css({'-moz-transform': rotate, 'transform' : rotate, '-webkit-transform': rotate, '-ms-transform': rotate,
-        "transition": "1s ease-out"});
-}
-
-    $(document).ready(function(){               
- 
-        $(".wing").on("mousedown", function(event) {
-            // console.log("PAGEX ON WING: " + event.pageX)
-        });
-
-        $('.wing').on('mousedown', function(event){
-            // get the orientation of the path (i.e. 0, 90, 180, -90)
-            str_ori = $(this).attr("id");
-            var ori = parseInt(str_ori, 10);
-            console.log("ORIENTATION: " + ori);
-            
-            // Get the id of the active attribute (will always be #marker)
-            active = $("#marker").attr("id");
-            // Do the rotation following the pointer
-            $('body').on('mousemove', function(event){
-                rotateAnnotationCropper($('#innerCircle').parent(), event.pageX,event.pageY, $('#marker'), ori);    
-            });
-
-            $('body').mouseup(function(event){
-                $('body').unbind('mousemove');
-                $('body').unbind('mouseup');
-                
-                var ang = getAngle(active);
-                var b = adjustToIncrement(ang, $('#marker'));
-                
-            });
-                                
-        });                   
-    }); 
+*/
